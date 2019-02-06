b0923689a304dbcde45466e7e3ce3666bf2ff38e
JDLong <jdlong1980@gmail.com>
2019-02-05 22:19:15 -0500

    updated javascript and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # liri-node-app
+LIRI is a CLI application that provides concert event info using the bands-in-town api, song information using the Spotify API, and movie information using the OMDB API.
+
+To get started, download or clone the repository.
+Navigate to the folder location and install the package.json: "npm install"
+Run the liri.js file using node from your command line (terminal, bash, etc): "node liri.js"
+
+## Menu Options
+A list of menu options will be displayed. Enter the number of your choice and press Enter.
+
+## Option 1 - Get concert details
+1. Enter an artist, group or band name when prompted.
+2. Choose a display option of list or table when prompted.
+
+## Option 2 - Get song details
+1. Enter a song title when prompted. Note: If no entry is made a default song will be chosen.
+2. Choose a display option of list or table when prompted.
+
+## Option 3 - Get movie details
+1. Enter a movie title when prompted. Note: If no entry is made a default move will be chosen.
+2. Choose a display option of list or table when prompted.
+
+## Option 4 - Get selection from random.txt file
+The random.txt file has an entry in the following format that will be read and executed:
+1. concert-this, <artist, group or band name>
+2. spotify-this-song, <song title>
+3. movie-this, <movie title>
+
+
--- liri.js
+++ liri.js
@@ -3,90 +3,257 @@ var keys = require('./keys.js');
 var axios = require('axios');
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
 var fs = require('fs');
 
 var searchTerm = process.argv.splice(3).join(' ');
+var display = 'list';
 
-var pickCase = function(caseData, functionData) {
-    switch (caseData) {
-        case 'concert-this':
-            getBand(functionData);
-            break;
-        case 'spotify-this-song':
-            getSpotify(functionData);
-            break;
-        case 'movie-this':
-            getMovie(functionData);
-            break;
-        case 'do-what-it-says':
-            doWhatItSays();
-            break;
-        default:
-            console.log('LIRI does not know that command!');
-    };
+var pickOption = function() {
+    console.log(chalk.blue('The following commands are available: \n') +
+        chalk.green('(1)') + ' Get concert details\n' +
+        chalk.green('(2)') + ' Get song details\n' +
+        chalk.green('(3)') + ' Get movie details\n' +
+        chalk.green('(4)') + ' Get selection from random.txt file');
+
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Please enter an option from the list above:',
+        name: 'option'
+    }]).then(function(response) {
+        switch (response.option) {
+            case '1':
+                getConcert();
+                break;
+            case '2':
+                getSpotify();
+                break;
+            case '3':
+                getMovie();
+                break;
+            case '4':
+                doWhatItSays();
+                break;
+            default:
+                console.log(chalk.yellow('LIRI does not know that command!'));
+                pickOption();
+        };
+    });
 };
 
 var log = function() {
     // Create a string of the entered commands
-    var text = moment().format('MM/DD/YYYY HH:mm') + ', Command: ' + process.argv[2] + ', Search: ' + searchTerm
+    var text = moment().format('MM/DD/YYYY HH:mm');
+    if (process.argv.length > 2) {
+        text += ', Command: ' + process.argv[2] + ', Search: ' + searchTerm
+    } else {
+        text += ', no user entry'
+    };
 
     // Log the commands if string not empty
-    if (text != '') {
-        fs.appendFile('log.txt', text + '\r', function(err) {
-            if (err) {
-                console.log(err);
-            };
-        });
-    };
+    fs.appendFile('log.txt', text + '\r', function(err) {
+        if (err) {
+            console.log(err);
+        };
+    });
 };
 
-var getBand = function(artist) {
-    axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp')
-        .then(function(response) {
-            var concerts = response.data;
-            for (var i = 0; i < concerts.length; i++) {
-                console.log('Venue: ' + concerts[i].venue.name);
-                console.log('Location: ' + concerts[i].venue.city + ', ' + concerts[i].venue.region + ' ' + concerts[i].venue.country);
-                console.log('Date: ' + moment(concerts[i].datetime.substring(0, 10), 'YYYY-MM-DD').format('MM/DD/YYYY'));
-                console.log('----------------------------------------');
-            };
+var getConcert = function() {
+    var artist;
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Please enter an artist, group or band:',
+        name: "artist"
+    }]).then(function(response) {
+        artist = response.artist;
+        inquirer.prompt([{
+            type: 'list',
+            message: 'Would you like the results displayed in a list or table?',
+            choices: ['list', 'table'],
+            name: 'display'
+        }]).then(function(response) {
+            display = response.display;
+            displayConcert(artist);
         });
+    });
+};
+
+var displayConcert = function(artist) {
+    if (!artist) {
+        console.log(chalk.yellow('No artist/band entered. Please try again.'));
+        restart();
+    } else {
+        axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp')
+            .then(function(response) {
+
+                var concerts = response.data;
+                if (concerts.length === 0) {
+                    console.log(chalk.red('No concert information found.'));
+                } else {
+
+                    if (display === 'list') {
+
+                        for (var i = 0; i < concerts.length; i++) {
+                            console.log(chalk.green('Venue: ') + concerts[i].venue.name);
+                            console.log(chalk.green('Location: ') + concerts[i].venue.city + ', ' + concerts[i].venue.region + ' ' + concerts[i].venue.country);
+                            console.log(chalk.green('Date: ') + moment(concerts[i].datetime.substring(0, 10), 'YYYY-MM-DD').format('MM/DD/YYYY'));
+                            console.log(chalk.yellow('----------------------------------------'));
+                        };
+
+                    } else { //table
+
+                        var table = new Table({
+                            head: [chalk.yellow('Venue'), chalk.yellow('Location'), chalk.yellow('Date')],
+                            colWidths: [30, 30, 15]
+                        });
+
+                        for (var i = 0; i < concerts.length; i++) {
+                            table.push(
+                                [concerts[i].venue.name, concerts[i].venue.city + ', ' + concerts[i].venue.region + ' ' + concerts[i].venue.country, moment(concerts[i].datetime.substring(0, 10), 'YYYY-MM-DD').format('MM/DD/YYYY')]
+                            );
+                        };
+
+                        console.log(table.toString());
+                    };
+                };
+                restart();
+            });
+    };
 };
 
 var getArtistNames = function(artist) {
     return artist.name;
 }
 
-var getSpotify = function(songName) {
+var getSpotify = function() {
+    var songName;
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Please enter a song title:',
+        name: "song"
+    }]).then(function(response) {
+        songName = response.song;
+        inquirer.prompt([{
+            type: 'list',
+            message: 'Would you like the results displayed in a list or table?',
+            choices: ['list', 'table'],
+            name: 'display'
+        }]).then(function(response) {
+            display = response.display;
+            displaySpotify(songName);
+        });
+    });
+};
+
+var displaySpotify = function(songName) {
+    if (!songName) {
+        songName = 'I Will Always Love You';
+        console.log(chalk.yellow('No song title entered. Showing results for "I Will Always Love You" by Dolly Parton.'));
+    };
+
     var spotify = new Spotify(keys.spotify);
 
-    if (!songName) { var songName = 'the sign' };
     spotify.search({ type: 'track', query: songName })
         .then(function(response) {
             var songs = response.tracks.items;
-            for (var i = 0; i < songs.length; i++) {
-                console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
-                console.log('song name: ' + songs[i].name);
-                console.log('preview song: ' + songs[i].preview_url);
-                console.log('album: ' + songs[i].album.name);
-                console.log('----------------------------------------');
+
+            if (songs.length === 0) {
+                console.log(chalk.red('No song information found.'));
+            } else {
+                if (display === 'list') {
+
+                    for (var i = 0; i < songs.length; i++) {
+                        console.log(chalk.green('artist(s): ') + songs[i].artists.map(getArtistNames).join(', '));
+                        console.log(chalk.green('song name: ') + songs[i].name);
+                        console.log(chalk.green('preview song: ') + songs[i].preview_url);
+                        console.log(chalk.green('album: ') + songs[i].album.name);
+                        console.log(chalk.yellow('----------------------------------------'));
+                    };
+
+                } else { //table
+
+                    var table = new Table({
+                        head: [chalk.yellow('Artist(s)'), chalk.yellow('Song'), chalk.yellow('Album'), chalk.yellow('Preview')],
+                        colWidths: [30, 30, 30, 80]
+                    });
+
+                    for (var i = 0; i < songs.length; i++) {
+                        var preview = songs[i].preview_url;
+                        if (!preview) { preview = 'n/a' };
+                        table.push(
+                            [songs[i].artists.map(getArtistNames).join(', '), songs[i].name, songs[i].album.name, preview]
+                        );
+                    };
+
+                    console.log(table.toString());
+
+                };
             };
+            restart();
         });
 };
 
-var getMovie = function(movieName) {
-    if (!movieName) { var movieName = 'mr nobody' };
+var getMovie = function() {
+    var movieName;
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Please enter a movie title:',
+        name: "movie"
+    }]).then(function(response) {
+        movieName = response.movie;
+        inquirer.prompt([{
+            type: 'list',
+            message: 'Would you like the results displayed in a list or table?',
+            choices: ['list', 'table'],
+            name: 'display'
+        }]).then(function(response) {
+            display = response.display;
+            displayMovie(movieName);
+        });
+    });
+};
+
+var displayMovie = function(movieName) {
+    if (!movieName) {
+        movieName = 'mr nobody';
+        console.log(chalk.yellow('No movie title entered. Showing results for "Mr. Nobody".'));
+    };
+
     axios.get('http://www.omdbapi.com/?apikey=trilogy&t=' + movieName)
         .then(function(response) {
             var movie = response.data;
 
-            console.log('Title: ' + movie.Title);
-            console.log('Year: ' + movie.Year);
-            console.log('Rated: ' + movie.Rated);
-            console.log('Country: ' + movie.Country);
-            console.log('Language: ' + movie.Language);
-            console.log('Plot: ' + movie.Plot);
-            console.log('Actors: ' + movie.Actors);
+            if (movie.length === 0) {
+                console.log(chalk.red('No movie information found.'));
+            } else {
+                if (display === 'list') {
+
+                    console.log(chalk.green('Title: ') + movie.Title);
+                    console.log(chalk.green('Year: ') + movie.Year);
+                    console.log(chalk.green('Rated: ') + movie.Rated);
+                    console.log(chalk.green('Country: ') + movie.Country);
+                    console.log(chalk.green('Language: ') + movie.Language);
+                    console.log(chalk.green('Actors: ') + movie.Actors);
+                    console.log(chalk.green('Plot: ') + movie.Plot);
+
+                } else { //table
+
+                    var table = new Table({
+                        head: [chalk.yellow('Title'), chalk.yellow('Year'), chalk.yellow('Rated'), chalk.yellow('Cntry'), chalk.yellow('Lang'), chalk.yellow('Actors'), chalk.yellow('Plot')],
+                        colWidths: [30, 8, 8, 8, 10, 30, 80]
+                    });
+
+                    table.push(
+                        [movie.Title, movie.Year, movie.Rated, movie.Country, movie.Language, movie.Actors, movie.Plot]
+                    );
+
+                    console.log(table.toString());
+
+                };
+            };
+            restart();
         });
 };
 
@@ -96,13 +263,46 @@ var doWhatItSays = function() {
 
         var dataArr = data.split(', ');
         if (dataArr[0] === '') {
-            console.log('The file does not contain a command.');
+            console.log(chalk.yellow('The file does not contain a command.'));
+            restart();
         } else {
-            pickCase(dataArr[0], dataArr[1]);
+            console.log(chalk.blue('The file contains: ' + chalk.yellow(data)));
+            switch (dataArr[0]) {
+                case 'concert-this':
+                    displayConcert(dataArr[1]);
+                    break;
+                case 'spotify-this-song':
+                    displaySpotify(dataArr[1]);
+                    break;
+                case 'movie-this':
+                    displayMovie(dataArr[1]);
+                    break;
+                case 'do-what-it-says':
+                    console.log(chalk.red('do-what-it-says cannot be used in the random.txt file! It will break me!'));
+                    break;
+                default:
+                    console.log(chalk.yellow('LIRI does not know that command!'));
+                    pickOption();
+            };
         };
 
     });
 };
 
-log();
-pickCase(process.argv[2], searchTerm);
+var restart = function() {
+    inquirer.prompt([{
+        type: "confirm",
+        message: "Would you like to choose another option?",
+        name: "confirm",
+        default: true
+
+    }]).then(function(response) {
+
+        if (response.confirm) {
+            pickOption();
+        };
+
+    });
+};
+
+pickOption();
